eec70c12ded12e9edc95b3cd11955ad6c7ca8a37
Mujtaba18 <mujtab189@gmail.com>
2024-10-20 21:12:59 +0300

    Styling the headings and body

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom"
 
 const Header = ({ user, handleLogOut }) => {
   let userOptions
+  let welcomeMsg
 
   // Check if user is defined and has a role
   if (user) {
     if (user.role === "user") {
       userOptions = (
         <nav>
-          <h3>Welcome {user.name}!</h3>
           <Link to="/">Home</Link>
           <Link to="/">Hotels</Link>
           <Link to="/">My Booking</Link>
@@ -20,7 +20,6 @@ const Header = ({ user, handleLogOut }) => {
     } else if (user.role === "admin") {
       userOptions = (
         <nav>
-          <h3>Welcome {user.name}! Admin</h3>
           <Link to="/">Home</Link>
           <Link to="/hotels">Hotels</Link>
           <Link to="/add-hotel">Add Hotel</Link>
@@ -37,7 +36,6 @@ const Header = ({ user, handleLogOut }) => {
     // Provide a default greeting when user is not logged in
     userOptions = (
       <nav>
-        <h3>Welcome Guest!</h3>
         <Link to="/">Home</Link>
         <Link to="/register">Register</Link>
         <Link to="/signin">Sign In</Link>
@@ -45,13 +43,24 @@ const Header = ({ user, handleLogOut }) => {
     )
   }
 
+  if (user) {
+    if (user.role === "user") {
+      welcomeMsg = <p className="welcomeMsg">Welcome {user.name}!</p>
+    } else if (user.role === "admin") {
+      welcomeMsg = <p className="welcomeMsg">Welcome {user.name}! <span>-Admin-</span></p>
+    }
+  } else {
+    welcomeMsg = <p className="welcomeMsg">Welcome Guest!</p>
+  }
+
   return (
     <header>
       <Link to="/">
         <div className="logo-wrapper" alt="logo">
-          LOGO
+          <img src="https://i.postimg.cc/TP6D3xrK/Velvet-Hotels-logo.png" alt="Logo" width="50px" height="50px" />
         </div>
       </Link>
+      {welcomeMsg}
       {userOptions}
     </header>
   )
--- src/pages/Hotels.jsx
+++ src/pages/Hotels.jsx
@@ -1,31 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react"
 
 const Hotels = () => {
   const [hotels, setHotels] = useState([])
 
   useEffect(() => {
-    fetch('http://localhost:3001/hotels/getHotels')
+    fetch("http://localhost:3001/hotels/getHotels")
       .then((res) => res.json())
       .then((data) => setHotels(data))
-      .catch((error) => console.error('Error fetching hotels:', error))
+      .catch((error) => console.error("Error fetching hotels:", error))
   }, [])
 
   return (
     <div>
       <h1>Hotels</h1>
       {hotels.length > 0 ? (
-        <ul>
+        <>
           {hotels.map((hotel) => (
-            <li key={hotel._id}>
+            <div key={hotel._id}>
               <h2>{hotel.hotel_name}</h2>
               <img src={hotel.hotel_image} alt={hotel.hotel_name} width="300" />
               <p>{hotel.hotel_location}</p>
               <p>{hotel.hotel_description}</p>
               <p>Price: ${hotel.hotel_price}</p>
               <p>Rating: {hotel.hotel_rating}</p>
-            </li>
+            </div>
           ))}
-        </ul>
+        </>
       ) : (
         <p>No hotels found.</p>
       )}
