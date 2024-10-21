9151b756a7371e5ba469183d946f9ef235da4208
H-aliwi <hussainjaffer84@gmail.com>
2024-10-21 04:49:50 -0700

    display hotel details done

--- src/pages/HotelDetails.jsx
+++ src/pages/HotelDetails.jsx
@@ -14,7 +14,7 @@ const HotelDetails = () => {
         const response = await axios.get(
           `http://localhost:3001/hotels/details/${hotelId}`
         )
-        setHotelDetails(response.data)
+        setHotelDetails(response.data) // store data in HotelDetails
         console.log(response)
       } catch (error) {
         throw error
@@ -26,7 +26,32 @@ const HotelDetails = () => {
 
   return (
     <>
-      <h1>HI</h1>
+      {HotelDetails ? (
+        <div className="">
+          <div className="" key={HotelDetails._id}>
+            <h2>{HotelDetails.hotel_name}</h2>
+            <img
+              src={HotelDetails.hotel_image}
+              alt={HotelDetails.hotel_name}
+              width="300"
+            />
+            <p>
+              <strong>Location:</strong> {HotelDetails.hotel_location}
+            </p>
+            <p>
+              <strong>Description:</strong> {HotelDetails.hotel_description}
+            </p>
+            <p>
+              <strong>Price:</strong> ${HotelDetails.hotel_price}
+            </p>
+            <p>
+              <strong>Rating:</strong> {HotelDetails.hotel_rating}
+            </p>
+          </div>
+        </div>
+      ) : (
+        <p>No hotels found.</p>
+      )}
     </>
   )
 }
