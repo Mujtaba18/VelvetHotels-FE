6829dd54dc23f4e417cd0c48f4da9771f1e4d407
Mujtaba18 <mujtab189@gmail.com>
2024-10-21 15:53:48 +0300

    Modified the amenities with css and list size

--- src/pages/Hotels.jsx
+++ src/pages/Hotels.jsx
@@ -27,22 +27,27 @@ const Hotels = () => {
         <div className="hotels-list">
           {hotels.map((hotel) => (
             <div className="hotels-card" key={hotel._id}>
-              <img className="hotels-img" src={`http://localhost:3001/${hotel.hotel_image}`} alt={hotel.hotel_name} width="300" />
-              <div className="hotels-info">
-                <h2>{hotel.hotel_name}</h2>
-                <p>Location: {hotel.hotel_location}</p>
-                <p>Description: {hotel.hotel_description}</p>
-                <div className="hotels-info-n">
-                  <p>Price: ${hotel.hotel_price}</p>
-                  <p>Rooms: {hotel.hotel_rooms}</p>
-                  <p>Stars: {hotel.hotel_stars}</p>
-                  <p>Rating: {hotel.hotel_rating}</p>
+              <div className="hotels-img-info">
+                <img className="hotels-img" src={`http://localhost:3001/${hotel.hotel_image}`} alt={hotel.hotel_name} width="300" />
+                <div className="hotels-info">
+                  <h2>{hotel.hotel_name}</h2>
+                  <p>Location: {hotel.hotel_location}</p>
+                  <p>Description: {hotel.hotel_description}</p>
+                  <div className="hotels-info-n">
+                    <p>Price: ${hotel.hotel_price}</p>
+                    <p>Rooms: {hotel.hotel_rooms}</p>
+                    <p>Stars: {hotel.hotel_stars}</p>
+                    <p>Rating: {hotel.hotel_rating}</p>
+                  </div>
                 </div>
-                <div>
-                  {hotel.amenities.map((amenity) => (
-                    <p>Amenity: {amenity.amenity_name}</p>
-                  ))}
-                </div>
+              </div>
+              <div className="amenity-list">
+                    {hotel.amenities.map((amenity) => (
+                      <div className="amenity-card">
+                        <img src={`http://localhost:3001/${amenity.amenity_icon}`} alt="AmenityIcon" width="30px" height="30px" />
+                        <p>{amenity.amenity_name}</p>
+                      </div>
+                    ))}
               </div>
             </div>
           ))}
