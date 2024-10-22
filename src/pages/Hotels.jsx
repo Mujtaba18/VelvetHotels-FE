398f38ebbd245be9983ff00174ce39d97e51310a
Mujtaba18 <mujtab189@gmail.com>
2024-10-22 22:24:06 +0300

    Differentiate the hotels listing based on user role

--- src/pages/Hotels.jsx
+++ src/pages/Hotels.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react"
 import axios from "axios"
 import { Link } from "react-router-dom"
 
-const Hotels = ({user}) => {
+const Hotels = ({ user }) => {
   const [hotels, setHotels] = useState([])
   const [sortOrder, setSortOrder] = useState("low-high") //useState to handle sort order
   const [sortBy, setSortBy] = useState("price") // useState to manage what to sort
@@ -39,118 +39,220 @@ const Hotels = ({user}) => {
   })
 
   return (
-    <div>
-      <h1>Hotels</h1>
-      <div className="sort-list">
-        <label htmlFor="sortBy">Sort by:</label>
-
-        <select
-          id="sortBy"
-          value={sortBy}
-          onChange={(e) => setSortBy(e.target.value)} //handel change the sort
-        >
-          <option value="price">Price</option>
-          <option value="rating">Rating</option>
-        </select>
-        <label htmlFor="sortOrder">Order:</label>
-        <select
-          id="sortOrder"
-          value={sortOrder}
-          onChange={(e) => setSortOrder(e.target.value)} //handel change the order
-        >
-          <option value="low-high">Low to High</option>
-          <option value="high-low">High to Low</option>
-        </select>
-      </div>
-
-      {sortedHotels.length > 0 ? (
-        <div className="hotels-list">
-          {sortedHotels.map((hotel) => (
-            <Link
-              to={`/hotels/details/${hotel._id}`}
-              key={hotel._id}
-              className="link-hotel"
-            >
-              <div className="hotels-card" key={hotel._id}>
-                <div className="hotels-img-info">
-                  <img
-                    className="hotels-img"
-                    src={`http://localhost:3001/${hotel.hotel_image}`}
-                    alt={hotel.hotel_name}
-                    width="300"
-                  />
-                  <div className="hotels-info">
-                    <div className="hotels-name">
-                      <h2>{hotel.hotel_name}</h2>
-                      <p>
-                        {hotel.hotel_stars === 5 ? (
-                          <>
-                            <span>⭐⭐⭐⭐⭐</span>
-                          </>
-                        ) : hotel.hotel_stars === 4 ? (
-                          <>
-                            <span>⭐⭐⭐⭐</span>
-                          </>
-                        ) : hotel.hotel_stars === 3 ? (
-                          <>
-                            <span>⭐⭐⭐</span>
-                          </>
-                        ) : hotel.hotel_stars === 2 ? (
-                          <>
-                            <span>⭐⭐</span>
-                          </>
-                        ) : hotel.hotel_stars === 1 ? (
-                          <span>⭐</span>
-                        ) : (
-                          <span>Not Rated</span>
-                        )}
-                      </p>
-                    </div>
-                    <div className="hotels-info-n">
-                      <p>
-                        <strong>Location: </strong> {hotel.hotel_location}
-                      </p>
-                      <p>
-                        <strong>Price: </strong> ${hotel.hotel_price}
-                      </p>
-                      <p>
-                        <strong>Rooms: </strong> {hotel.hotel_rooms}
-                      </p>
-                      <p>
-                        <strong>Rating:</strong>{" "}
-                        {hotel.hotel_rating.length > 0
-                          ? (
-                              hotel.hotel_rating.reduce(
-                                (acc, review) => acc + review.rating,
-                                0
-                              ) / hotel.hotel_rating.length
-                            ).toFixed(1)
-                          : "No ratings yet"}
-                      </p>
-                    </div>
-                  </div>
+    <>
+      {user ? (
+        <>
+          {user.role === "admin" ? (
+            <div>
+              <div>
+                <h1>Hotels</h1>
+                <div className="sort-list">
+                  <label htmlFor="sortBy">Sort by:</label>
+                  <select
+                    id="sortBy"
+                    value={sortBy}
+                    onChange={(e) => setSortBy(e.target.value)} // handle change the sort
+                  >
+                    <option value="price">Price</option>
+                    <option value="rating">Rating</option>
+                  </select>
+                  <label htmlFor="sortOrder">Order:</label>
+                  <select
+                    id="sortOrder"
+                    value={sortOrder}
+                    onChange={(e) => setSortOrder(e.target.value)} // handle change the order
+                  >
+                    <option value="low-high">Low to High</option>
+                    <option value="high-low">High to Low</option>
+                  </select>
                 </div>
-                <div className="amenity-list">
-                  {hotel.amenities.map((amenity) => (
-                    <div className="amenity-card">
-                      <img
-                        src={`http://localhost:3001/${amenity.amenity_icon}`}
-                        alt="AmenityIcon"
-                        width="30px"
-                        height="30px"
-                      />
-                      <p>{amenity.amenity_name}</p>
-                    </div>
-                  ))}
-                </div>
+
+                {sortedHotels.length > 0 ? (
+                  <table className="table table-striped container">
+                    <thead>
+                      <tr>
+                        <th>Hotel Name</th>
+                        <th>Location</th>
+                        <th>Price</th>
+                        <th>Rooms</th>
+                        <th>Rating</th>
+                        <th>Amenities</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {sortedHotels.map((hotel) => (
+                        <tr key={hotel._id}>
+                          <td>
+                            <Link
+                              to={`/hotels/details/${hotel._id}`}
+                              className="link-hotel"
+                            >
+                              {hotel.hotel_name}
+                            </Link>
+                          </td>
+                          <td>{hotel.hotel_location}</td>
+                          <td>${hotel.hotel_price}</td>
+                          <td>{hotel.hotel_rooms}</td>
+                          <td>
+                            {hotel.hotel_rating.length > 0
+                              ? (
+                                  hotel.hotel_rating.reduce(
+                                    (acc, review) => acc + review.rating,
+                                    0
+                                  ) / hotel.hotel_rating.length
+                                ).toFixed(1)
+                              : "No ratings yet"}
+                          </td>
+                          <td>
+                            {hotel.amenities.map((amenity) => (
+                              <div key={amenity._id} className="amenity-card">
+                                <img
+                                  src={`http://localhost:3001/${amenity.amenity_icon}`}
+                                  alt={amenity.amenity_name}
+                                  width="30"
+                                  height="30"
+                                />
+                                <span>{amenity.amenity_name}</span>
+                              </div>
+                            ))}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                ) : (
+                  <p>No hotels found.</p>
+                )}
               </div>
-            </Link>
-          ))}
-        </div>
+            </div>
+          ) : user.role === "user" ? (
+            <div>
+              <div>
+                <h1>Hotels</h1>
+                <div className="sort-list">
+                  <label htmlFor="sortBy">Sort by:</label>
+
+                  <select
+                    id="sortBy"
+                    value={sortBy}
+                    onChange={(e) => setSortBy(e.target.value)} //handel change the sort
+                  >
+                    <option value="price">Price</option>
+                    <option value="rating">Rating</option>
+                  </select>
+                  <label htmlFor="sortOrder">Order:</label>
+                  <select
+                    id="sortOrder"
+                    value={sortOrder}
+                    onChange={(e) => setSortOrder(e.target.value)} //handel change the order
+                  >
+                    <option value="low-high">Low to High</option>
+                    <option value="high-low">High to Low</option>
+                  </select>
+                </div>
+
+                {sortedHotels.length > 0 ? (
+                  <div className="hotels-list">
+                    {sortedHotels.map((hotel) => (
+                      <Link
+                        to={`/hotels/details/${hotel._id}`}
+                        key={hotel._id}
+                        className="link-hotel"
+                      >
+                        <div className="hotels-card" key={hotel._id}>
+                          <div className="hotels-img-info">
+                            <img
+                              className="hotels-img"
+                              src={`http://localhost:3001/${hotel.hotel_image}`}
+                              alt={hotel.hotel_name}
+                              width="300"
+                            />
+                            <div className="hotels-info">
+                              <div className="hotels-name">
+                                <h2>{hotel.hotel_name}</h2>
+                                <p>
+                                  {hotel.hotel_stars === 5 ? (
+                                    <>
+                                      <span>⭐⭐⭐⭐⭐</span>
+                                    </>
+                                  ) : hotel.hotel_stars === 4 ? (
+                                    <>
+                                      <span>⭐⭐⭐⭐</span>
+                                    </>
+                                  ) : hotel.hotel_stars === 3 ? (
+                                    <>
+                                      <span>⭐⭐⭐</span>
+                                    </>
+                                  ) : hotel.hotel_stars === 2 ? (
+                                    <>
+                                      <span>⭐⭐</span>
+                                    </>
+                                  ) : hotel.hotel_stars === 1 ? (
+                                    <span>⭐</span>
+                                  ) : (
+                                    <span>Not Rated</span>
+                                  )}
+                                </p>
+                              </div>
+                              <div className="hotels-info-n">
+                                <p>
+                                  <strong>Location: </strong>{" "}
+                                  {hotel.hotel_location}
+                                </p>
+                                <p>
+                                  <strong>Price: </strong> ${hotel.hotel_price}
+                                </p>
+                                <p>
+                                  <strong>Rooms: </strong> {hotel.hotel_rooms}
+                                </p>
+                                <p>
+                                  <strong>Rating:</strong>{" "}
+                                  {hotel.hotel_rating.length > 0
+                                    ? (
+                                        hotel.hotel_rating.reduce(
+                                          (acc, review) => acc + review.rating,
+                                          0
+                                        ) / hotel.hotel_rating.length
+                                      ).toFixed(1)
+                                    : "No ratings yet"}
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="amenity-list">
+                            {hotel.amenities.map((amenity) => (
+                              <div className="amenity-card">
+                                <img
+                                  src={`http://localhost:3001/${amenity.amenity_icon}`}
+                                  alt="AmenityIcon"
+                                  width="30px"
+                                  height="30px"
+                                />
+                                <p>{amenity.amenity_name}</p>
+                              </div>
+                            ))}
+                          </div>
+                        </div>
+                      </Link>
+                    ))}
+                  </div>
+                ) : (
+                  <p>No hotels found.</p>
+                )}
+              </div>
+            </div>
+          ) : (
+            <div>
+              <h2>What is this user role?</h2>
+            </div>
+          )}
+        </>
       ) : (
-        <p>No hotels found.</p>
+        <div>
+          <h2>Please log in.</h2>
+        </div>
       )}
-    </div>
+    </>
   )
 }
 
